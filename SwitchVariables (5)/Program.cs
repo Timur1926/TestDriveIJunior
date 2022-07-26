313b06f33a6daa408ebf51ba8a5eb567d71ee1eb
s0n1c_AK <alexkovtunenkogm@gmail.com>
2022-07-27 01:21:27 +0300

    Изменил код к 5 задачи

--- SwitchVariables (5)/Program.cs
+++ SwitchVariables (5)/Program.cs
@@ -8,10 +8,13 @@ class Program
         string playerName = "Kovtunenko";
         string playerSurname = "Alex";
 
+        Console.WriteLine($"Имя до перестановки {playerName}");
+        Console.WriteLine($"Фамилия до перестановки {playerSurname}");
+
         (playerName, playerSurname) = (playerSurname, playerName);
 
-        //Console.WriteLine(playerName);
-        //Console.WriteLine(playerSurname);
-        //Console.ReadLine();
+        Console.WriteLine($"\nИмя после перестановки {playerName}");
+        Console.WriteLine($"Фамилия после перестановки {playerSurname}");
+        Console.ReadLine();
     }
 }
